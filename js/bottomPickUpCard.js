0276cf316d421aca1c270aa3389cdecea4f19ed0
MiguelJerome <sharkmaycry@gmail.com>
2022-08-01 01:35:22 -0400

    added new animation

--- js/bottomPickUpCard.js
+++ js/bottomPickUpCard.js
@@ -6,16 +6,16 @@ var BottomPickUpCard ={
 
     ShowAndUpdateAllBottomPickupCard(boardCard1, boardCard2, boardCard3,  boardCard4)
     {
-    var buttonPickUpBottomPlayerCard1Text = "Pick-up " + boardCard1;
-    BottomPickUpCard._buttonPickUpBottomPlayerCard1.textContent = buttonPickUpBottomPlayerCard1Text;
+        var buttonPickUpBottomPlayerCard1Text = "Pick-up " + boardCard1;
+        BottomPickUpCard._buttonPickUpBottomPlayerCard1.textContent = buttonPickUpBottomPlayerCard1Text;
 
-    var buttonPickUpBottomPlayerCard2Text = "Pick-up " + boardCard2;
-    BottomPickUpCard._buttonPickUpBottomPlayerCard2.textContent = buttonPickUpBottomPlayerCard2Text;
+        var buttonPickUpBottomPlayerCard2Text = "Pick-up " + boardCard2;
+        BottomPickUpCard._buttonPickUpBottomPlayerCard2.textContent = buttonPickUpBottomPlayerCard2Text;
 
-    var buttonPickUpBottomPlayerCard3Text = "Pick-up " + boardCard3;
-    BottomPickUpCard._buttonPickUpBottomPlayerCard3.textContent = buttonPickUpBottomPlayerCard3Text;
+        var buttonPickUpBottomPlayerCard3Text = "Pick-up " + boardCard3;
+        BottomPickUpCard._buttonPickUpBottomPlayerCard3.textContent = buttonPickUpBottomPlayerCard3Text;
 
-    var buttonPickUpBottomPlayerCard4Text = "Pick-up " + boardCard4;
-    BottomPickUpCard._buttonPickUpBottomPlayerCard4.textContent = buttonPickUpBottomPlayerCard4Text;
+        var buttonPickUpBottomPlayerCard4Text = "Pick-up " + boardCard4;
+        BottomPickUpCard._buttonPickUpBottomPlayerCard4.textContent = buttonPickUpBottomPlayerCard4Text;
     }
 };
